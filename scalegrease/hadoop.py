0d64a7bcf268e8f4bef7d87975b906cbf602378b
Martins Kalvans <kalvans@spotify.com>
2014-06-19 15:49:34 +0200

    Fix HadoopRunner after refactoring artifacts

--- scalegrease/hadoop.py
+++ scalegrease/hadoop.py
@@ -5,7 +5,7 @@ from scalegrease import runner
 
 
 class HadoopRunner(runner.RunnerBase):
-    def run_job(self, jar_path, artifact, argv):
-        hadoop_cmd = self._config["command"] + [jar_path] + argv
+    def run_job(self, artifact_storage, argv):
+        hadoop_cmd = self._config["command"] + [artifact_storage.jar_path()] + argv
         logging.info("Executing: %s", " ".join(hadoop_cmd))
         subprocess.check_call(hadoop_cmd)
