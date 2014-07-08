683de55c0b8cc82f4dba4fb26028ea602922750f
Arash Rouhani <arash@spotify.com>
2014-07-08 14:33:18 +0200

    Linting

--- scalegrease/hadoop.py
+++ scalegrease/hadoop.py
@@ -4,5 +4,6 @@ from scalegrease import system
 
 class HadoopRunner(runner.RunnerBase):
     def run_job(self, artifact_storage, argv):
-        hadoop_cmd = self._config["command"] + [artifact_storage.jar_path()] + argv
+        hadoop_cmd = (self._config["command"] + [artifact_storage.jar_path()] +
+                      argv)
         system.run_with_logging(hadoop_cmd)
