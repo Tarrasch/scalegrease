c7136c0da9529a9632308e96c024c68ef203c01f
Arash Rouhani <arash@spotify.com>
2014-07-08 14:26:47 +0200

    Use run_with_logging (missed it in cf87e9b)

--- scalegrease/hadoop.py
+++ scalegrease/hadoop.py
@@ -1,11 +1,8 @@
-import logging
-import subprocess
-
 from scalegrease import runner
+from scalegrease import system
 
 
 class HadoopRunner(runner.RunnerBase):
     def run_job(self, artifact_storage, argv):
         hadoop_cmd = self._config["command"] + [artifact_storage.jar_path()] + argv
-        logging.info("Executing: %s", " ".join(hadoop_cmd))
-        subprocess.check_call(hadoop_cmd)
+        system.run_with_logging(hadoop_cmd)
