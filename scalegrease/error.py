e0977730c665e066b137083cd8613de58424c515
Arash Rouhani <arash@spotify.com>
2014-07-29 15:45:11 +0200

    Add docs to error.py
    fixes spotify/scalegrease#14

--- scalegrease/error.py
+++ scalegrease/error.py
@@ -1,2 +1,14 @@
+"""
+It is a common pattern for packages to have a user-defined exception hierarchy
+with a common base class, in order to make it easier to distinguish between
+package logic (i.e. scalegrease) errors and system errors. Error is the base
+class, and there are no children yet.
+
+See http://www.network-theory.co.uk/docs/pytut/UserdefinedExceptions.html for
+some examples.
+"""
+
+
 class Error(Exception):
+    """An exception from within scalegrease-code."""
     pass
